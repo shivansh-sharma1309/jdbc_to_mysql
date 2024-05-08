8c11a289b8c392c50f00949978405a9ce4485fab
shivansh <shivansh_sharma@persistent.com>
2024-05-08 13:05:31 +0530

    added code for jdbc connection to mysql

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+
+public class Main {
+        public static void main(String[] args) {
+                String url = "jdbc:mysql://4d275b38-2eee-4b4d-8a88-cb022388e975.blijti4d0v0nkr55oei0.databases.appdomain.cloud:32661/TM_IBM_POWER_DB_1";
+                String user = "TM_IBM_POWER_USER";
+                String password = "2WxKG*C-@QaTbor";
+
+                try (Connection conn = DriverManager.getConnection(url,user,password)) {
+                        System.out.println("Connected to the database!");
+                        DatabaseMetaData metaData = conn.getMetaData();
+                        String catalog = null ;
+                        String schemaPattern = null;
+
+                        ResultSet resultSet = metaData.getTables(catalog, schemaPattern, null, new String[] {"TABLE"});
+                        while(resultSet.next()){
+                                String tableName = resultSet.getString("TABLE_NAME");
+                                System.out.println(tableName);
+                        }
+                } catch (SQLException e) {
+                        System.err.println("Failed to connect to the database!");
+                        e.printStackTrace();
+                }
+        }
+}
