cfb2338886646fe3a0e1d582f4690bcb46ee30d4
shivansh <shivansh_sharma@persistent.com>
2024-05-08 13:06:50 +0530

    added code for jdbc connection to mysql

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ import java.sql.ResultSet;
 public class Main {
         public static void main(String[] args) {
                 String url = "jdbc:mysql://4d275b38-2eee-4b4d-8a88-cb022388e975.blijti4d0v0nkr55oei0.databases.appdomain.cloud:32661/TM_IBM_POWER_DB_1";
-                String user = "TM_IBM_POWER_USER";
-                String password = "2WxKG*C-@QaTbor";
+                String user = "Your username";
+                String password = "Your password";
 
                 try (Connection conn = DriverManager.getConnection(url,user,password)) {
                         System.out.println("Connected to the database!");
